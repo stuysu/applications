9556e6b52297950ddbd4418d17ef2042e0f04929
Abir Taheer <abir@taheer.me>
2020-10-26 04:24:32 -0400

    fixed migrations for acceptance messages

--- database/migrations/20201026000040-create-acceptance-messages.js
+++ database/migrations/20201026000040-create-acceptance-messages.js
@@ -1,7 +1,7 @@
 'use strict';
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('acceptance-messages', {
+    await queryInterface.createTable('acceptanceMessages', {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -28,6 +28,6 @@ module.exports = {
     });
   },
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.dropTable('acceptance-messages');
+    await queryInterface.dropTable('acceptanceMessages');
   }
 };
